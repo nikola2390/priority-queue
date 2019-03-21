a198265b18589ed07e24b2fccecc3ca235d67776
Mikalai Shalupin <nikolas_23@tut.by>
2019-03-22 00:11:03 +0300

    feat: implement class Queue

--- src/queue.js
+++ src/queue.js
@@ -1,25 +1,36 @@
 const MaxHeap = require('./max-heap.js');
 
-class PriorityQueue {
-	constructor(maxSize) {
-
+class Queue {
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap();
+		this.length = 0;
 	}
 
 	push(data, priority) {
-
+		if (this.length === this.maxSize) {
+			throw new Error();
+		}
+		this.heap.push(data, priority);
+		this.length += 1;
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error();
+		}
+		let removed = this.heap.pop();
+		this.length -= 1;
+		return removed;
 	}
 
 	size() {
-
+		return this.length;
 	}
 
 	isEmpty() {
-		
+		return !this.length;
 	}
 }
 
-module.exports = PriorityQueue;
+module.exports = Queue;
